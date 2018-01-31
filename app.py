55543c6bfa7b37bade8e1c5bb0ab6a992f5daee7
Andrew Obrigewitsch <blazestudios23@gmail.com>
2018-01-30 16:38:39 -0800

    'b'

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ if __name__ == '__main__':
     if app.config['DEBUG']:
         @app.before_first_request
         def create_tables():
-            db.create_all()
+            db.create_all() 
 
     app.run(port=5000)
